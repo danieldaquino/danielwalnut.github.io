706fa3ead788fe4f7926daace85aa21c47571bd3
Daniel Walnut <danywalnut@gmail.com>
2016-12-17 20:44:41 -0800

    Added half version of Corbin's article content

---
+++ articles/CorbinSite/Old50s/index_resources/resources.js
@@ -0,0 +1,10 @@
+function LoadResources()
+{
+	TheHeadLines.push(new HeadLine("index_resources/1.jpg", "Another album is out!", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/6.jpg", "New album is out!", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/3.png", "Check out my pics!", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/4.jpg", "Thumbs up for my songs", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/1.jpg", "New song is out!", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/6.jpg", "Daniel is so handsome!", "index.html"));
+	TheHeadLines.push(new HeadLine("index_resources/3.png", "ENJOY!!", "index.html"));
+}
