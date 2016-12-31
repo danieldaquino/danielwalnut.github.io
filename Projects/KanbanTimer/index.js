402063fc59c812f1e4d797a19152b14e64211ba1
Daniel Walnut <me@danielwalnut.com>
2016-12-31 01:51:59 -0800

    Tried to code all logic at once. Let's see.

--- Projects/KanbanTimer/index.js
+++ Projects/KanbanTimer/index.js
@@ -1,17 +1,89 @@
-function Next() {
+var Tasks = new Array();
 
+Tasks.push(new Task("Do Schematics"));
+Tasks.push(new Task("Wire the Circuit"));
+Tasks.push(new Task("Troubleshoot"));
+Tasks.push(new Task("Fill in truth table"));
+
+TasksCursor = 0;
+
+var Task = function(Name) {
+	var that = this;
+	that.Done = false;
+	that.Name = Name;
+	that.Time = 0;
+	that.Obj = document.createElement("div");
+	that.Obj.classList.add("QueueObject");
+	that.Obj.innerText = that.Name;
+	document.getElementById("LeftQueue").appendChild(that.Obj);
+	that.Do = function() {
+		if(!that.Done) {
+			document.getElementById("LeftQueue").removeChild(that.Obj);
+			document.getElementById("RightQueue").appendChild(that.Obj);
+			that.Done = true;
+		}
+	}
+	that.Undo = function() {
+	if(that.Done) {
+		document.getElementById("RightQueue").removeChild(that.Obj);
+		document.getElementById("LeftQueue").appendChild(that.Obj);
+		that.Done = false;
+	}
+	}
+}
+
+function Next() {
+	if(TasksCursor != Tasks.length - 1) {
+		Tasks[TaskCursor].Do();
+		TasksCursor++;
+		DrawMainTask();
+	}
 }
 
 function Previous() {
-
+	if(TasksCursor != 0) {
+		Tasks[TaskCursor].Undo();
+		TasksCursor--;
+		DrawMainTask();
+	}
 }
 
 function DrawClock() {
+	var seconds = Tasks[TaskCursor].Time%60;
+	var minutes = (Math.floor(Tasks[TaskCursor].Time/60)%60);
+	var hours = Math.floor(Tasks[TaskCursor].Time/60);
+	if(seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	if(minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if(hours < 10) {
+		hours = "0" + hours;
+	}
+	document.getElementById("TimerObj").innerText = hours + ":" + minutes + ":" + seconds;
+}
 
+function Tick() {
+	Tasks[TaskCursor].Time++;
 }
 
 function DrawMainTask() {
-
+	document.getElementById("MainTaskObj").innerText = Tasks[TaskCursor].Name;
 }
 
-function Draw
+function DoKey(e) {
+	switch(e.keyCode) {
+		case 39:
+			Next();
+			break;
+		case 37:
+			Previous();
+			break;
+		
+	}
+}
+
+function Load() {
+	setInterval(Tick, 1000);	
+}
